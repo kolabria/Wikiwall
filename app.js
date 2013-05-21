fe0d8b789c972253906dec66e32b81c67983aa91
root <root@elm.(none)>
2013-05-21 15:23:14 -0400

    fix to ssl - remove passkey

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ var users = {};
 var sslPath = process.env.APP_PATH || '';
 //var ssDir = __dirname + '/public/uploads/';
 var options = {
- // key: fs.readFileSync(sslPath+'privatekey.pem'),
- // cert: fs.readFileSync(sslPath+'certificate.pem')
- ca:   fs.readFileSync(__dirname+'/ssl/sub.class1.server.ca.pem'),
- key:  fs.readFileSync(__dirname+'/ssl/ssl.key'),
- cert: fs.readFileSync(__dirname+'/ssl/ssl.crt')
+  //key: fs.readFileSync(sslPath+'privatekey.pem'),
+  //cert: fs.readFileSync(sslPath+'certificate.pem')
+  ca:   fs.readFileSync(__dirname+'/ssl/sub.class1.server.ca.pem'),
+  key:  fs.readFileSync(__dirname+'/ssl/ssl.key'),
+  cert: fs.readFileSync(__dirname+'/ssl/ssl.crt')
 };
 
 var app = module.exports = express.createServer(options);
