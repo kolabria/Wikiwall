101756a9668ec3be796115db2da89f188d48ebad
Ted Judge <tedjudge@kolabria.com>
2013-05-23 15:22:28 -0400

    more debug

--- app.js
+++ app.js
@@ -605,7 +605,7 @@ app.post('/ulogin', function(req, res){
 		if (user){
 			console.log('ulogin - cannot find user: ',req.body.user.Email);
 		}
-		else console.log('ulogin - invalid authentication: 'req.body.user.Email);
+		else console.log('ulogin - invalid authentication: ',req.body.user.Email);
 	  	user = {}
 	  	res.local('layout', 'sitelayout');
   		res.local('title', 'Kolbria - User Login')
